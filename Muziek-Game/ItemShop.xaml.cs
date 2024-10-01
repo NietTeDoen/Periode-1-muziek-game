cfda42945ac361baa95d42c12ffc696dfc4bb867
Harm Haandrikman <hfhaandrikman@gmail.com>
2024-10-01 20:37:47 +0200

    profiel, shop en menu is verbeterd

--- Muziek-Game/ItemShop.xaml.cs
+++ Muziek-Game/ItemShop.xaml.cs
@@ -1,70 +1,23 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 
 namespace Muziek_Game
 {
     public partial class ItemShop : UserControl
     {
-        public ObservableCollection<Item> Items { get; set; }
-
         public ItemShop()
         {
             InitializeComponent();
-            LoadItems();
-            DataContext = this;
         }
 
-        private void LoadItems()
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
-            Items = new ObservableCollection<Item>
+            // Ga terug naar het hoofdmenu
+            var window = Window.GetWindow(this) as MainWindow;
+            if (window != null)
             {
-                new Item { Name = "Outfit 1", Price = "1500 V-Bucks", Image = "path_to_image" },
-                new Item { Name = "Pickaxe 1", Price = "800 V-Bucks", Image = "path_to_image" },
-                new Item { Name = "Glider 1", Price = "1200 V-Bucks", Image = "path_to_image" }
-            };
-
-            ItemList.ItemsSource = Items;
-        }
-    }
-
-    public class Item
-    {
-        public string Name { get; set; }
-        public string Price { get; set; }
-        public string Image { get; set; }
-        public ICommand BuyCommand { get; set; }
-
-        public Item()
-        {
-            BuyCommand = new RelayCommand(BuyItem);
-        }
-
-        private void BuyItem(object parameter)
-        {
-            // Voeg hier de kooplogica toe
-            MessageBox.Show($"You bought {Name} for {Price}!");
-        }
-    }
-
-    public class RelayCommand : ICommand
-    {
-        private readonly Action<object> _execute;
-
-        public RelayCommand(Action<object> execute)
-        {
-            _execute = execute;
-        }
-
-        public event EventHandler CanExecuteChanged;
-
-        public bool CanExecute(object parameter) => true;
-
-        public void Execute(object parameter)
-        {
-            _execute(parameter);
+                window.MainContent.Content = new MainMenu();
+            }
         }
     }
 }
--- Muziek-Game/MainMenu.xaml.cs
+++ Muziek-Game/MainMenu.xaml.cs
@@ -17,31 +17,37 @@ namespace Muziek_Game
 
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
-            // Navigeer naar het instellingenmenu
             var window = Window.GetWindow(this) as MainWindow;
             if (window != null)
             {
-                window.MainContent.Content = new Settings(); // Zorg ervoor dat je een Settings UserControl hebt
+                window.MainContent.Content = new Settings();
             }
         }
 
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            // Laad de game
             var mainWindow = Window.GetWindow(this) as MainWindow;
             if (mainWindow != null)
             {
-                mainWindow.MainContent.Content = new GameControl(); // Vervang het menu door de game
+                mainWindow.MainContent.Content = new GameControl();
             }
         }
 
         private void ShopButton_Click(object sender, RoutedEventArgs e)
         {
-            // Laad de item shop
-            var mainWindow = Window.GetWindow(this) as MainWindow;
-            if (mainWindow != null)
+            var window = Window.GetWindow(this) as MainWindow;
+            if (window != null)
             {
-                mainWindow.MainContent.Content = new ItemShop(); // Vervang het menu door de winkel
+                window.MainContent.Content = new ItemShop();
+            }
+        }
+
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this) as MainWindow;
+            if (window != null)
+            {
+                window.MainContent.Content = new ProfilePage(); // Zorg ervoor dat deze UserControl bestaat
             }
         }
     }
