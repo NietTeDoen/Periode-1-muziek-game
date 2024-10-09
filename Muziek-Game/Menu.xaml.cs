18e3edfa6fa1720562c766b0aaf188da0e060f0d
ALI <ali.abdala@student.nhlstenden.com>
2024-10-09 14:38:01 +0200

    main menu muziek

--- Muziek-Game/Menu.xaml.cs
+++ Muziek-Game/Menu.xaml.cs
@@ -7,19 +7,10 @@ namespace Muziek_Game
 {
     public partial class Menu : Window
     {
-        private MediaPlayer _mediaPlayer;
         public Menu() 
         {
             MainContent.Content = new MainMenu(); // Start met het hoofdmenu
-            _mediaPlayer = new MediaPlayer();
             InitializeComponent();
-        }
-
-        private void PlayMusic_Click(object sender, RoutedEventArgs e)
-        {
-            Uri musicUri = new Uri("pack://application:,,,/Assets/muziek/ABBA.mp3");
-            _mediaPlayer.Open(musicUri);
-            _mediaPlayer.Play();
         }
 
         public void SwitchToSettings()
