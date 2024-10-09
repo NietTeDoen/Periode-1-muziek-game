be8dd554dfbd19a8f380973d997bfd9d8f0da88d
ALI <ali.abdala@student.nhlstenden.com>
2024-10-09 14:15:59 +0200

    halve functie

--- Muziek-Game/Menu.xaml.cs
+++ Muziek-Game/Menu.xaml.cs
@@ -1,14 +1,25 @@
 ﻿using Muziek_Game.Properties;
+using System;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Muziek_Game
 {
     public partial class Menu : Window
     {
-        public Menu()
+        private MediaPlayer _mediaPlayer;
+        public Menu() 
         {
-            InitializeComponent();
             MainContent.Content = new MainMenu(); // Start met het hoofdmenu
+            _mediaPlayer = new MediaPlayer();
+            InitializeComponent();
+        }
+
+        private void PlayMusic_Click(object sender, RoutedEventArgs e)
+        {
+            Uri musicUri = new Uri("pack://application:,,,/Assets/muziek/ABBA.mp3");
+            _mediaPlayer.Open(musicUri);
+            _mediaPlayer.Play();
         }
 
         public void SwitchToSettings()
