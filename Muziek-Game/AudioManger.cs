0157017e0927523c5a533201dc8d47b420831b89
ALI <ali.abdala@student.nhlstenden.com>
2024-10-10 19:38:39 +0200

    volume changer toegevoegde

---
+++ Muziek-Game/AudioManger.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Media;
+using System;
+
+public class AudioManager
+{
+    private static MediaPlayer _mediaPlayer;
+
+    // Zorg ervoor dat er slechts één MediaPlayer is
+    public static MediaPlayer GetMediaPlayer()
+    {
+        _mediaPlayer = new MediaPlayer();
+
+        // Gebruik een reguliere bestands-URI
+        string musicPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "muziek", "ABBA.mp3");
+        Uri musicUri = new Uri(musicPath);
+
+        _mediaPlayer.Volume = 0.5; // Maximaal volume
+        _mediaPlayer.Open(musicUri);
+        _mediaPlayer.Play();
+
+        return _mediaPlayer;
+    }
+
+    public int VolumeChange(double volume)
+    {
+        _mediaPlayer.Volume = volume;
+        return 1;
+    }
+}
